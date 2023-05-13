8b286377200033cefac2bcad447ca93ce014cba3
Thomas Garandel <tgarfr@laposte.net>
2023-05-13 23:27:17 +0200

    Adapt the project for compilation with recent gradle

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.estate.com.R;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewMain;
--- app/src/main/java/com/openclassrooms/realestatemanager/SecondActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/SecondActivity.java
@@ -3,6 +3,8 @@ package com.openclassrooms.realestatemanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.estate.com.R;
+
 public class SecondActivity extends AppCompatActivity {
 
     @Override
