bcc3dcca2c9d553b8328249019a8b332114935ff
Thomas Garandel <tgarfr@laposte.net>
2023-07-06 14:35:53 +0200

    Improve Room code

--- app/src/main/java/com/openclassrooms/realestatemanager/api/Database.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/api/Database.kt
@@ -1,13 +1,7 @@
 package com.openclassrooms.realestatemanager.api
 
-import android.content.Context
-import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.openclassrooms.realestatemanager.model.Estate
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @androidx.room.Database(entities = [Estate::class], version = 1)
 abstract class Database: RoomDatabase() {
@@ -15,26 +9,7 @@ abstract class Database: RoomDatabase() {
     abstract fun estateDao(): EstateDao
 
     companion object {
-        private const val DATABASE_NAME: String = "RealEstateManagerDatabase"
-
-        @Volatile
-        private var INSTANCE: Database? = null
-
-        fun getInstance(context: Context): Database =
-            INSTANCE ?: synchronized(this) {
-                INSTANCE ?: databaseBuilder(context.applicationContext, Database::class.java, DATABASE_NAME)
-                    .addCallback(prepopulateDatabase)
-                    .build().also { INSTANCE = it }
-            }
-
-        private val prepopulateDatabase = object : Callback() {
-            override fun  onCreate(db: SupportSQLiteDatabase) {
-                super.onCreate(db)
-                CoroutineScope(Dispatchers.IO).launch {
-                    INSTANCE?.estateDao()?.addEstateList(FakeEstateApi().getEstateList())
-                }
-            }
-        }
+        const val DATABASE_NAME: String = "RealEstateManagerDatabase"
     }
 
 }
--- app/src/main/java/com/openclassrooms/realestatemanager/repository/EstateRepository.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/repository/EstateRepository.kt
@@ -2,49 +2,27 @@ package com.openclassrooms.realestatemanager.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asLiveData
 import com.openclassrooms.realestatemanager.api.EstateApi
 import com.openclassrooms.realestatemanager.model.Estate
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class EstateRepository private constructor(private val estateApi: EstateApi) {
+class EstateRepository(private val estateApi: EstateApi) {
 
-    private val estateListLiveData = MutableLiveData<List<Estate>>(null)
+    private val estateListLiveData = estateApi.getEstateListFlow().asLiveData()
     private val selectedEstateLiveData: MutableLiveData<Estate> = MutableLiveData<Estate>()
-
-    init {
-        CoroutineScope(Dispatchers.IO).launch {
-            estateListLiveData.postValue(estateApi.getEstateList())
-        }
-    }
-
-    companion object {
-        @Volatile
-        private var INSTANCE: EstateRepository? = null
-
-        fun getInstance(estateApi: EstateApi): EstateRepository =
-            INSTANCE ?: synchronized(this) {
-                INSTANCE ?: EstateRepository(estateApi).also { INSTANCE = it }
-            }
-    }
-
 
     fun getEstateListLiveData(): LiveData<List<Estate>> = estateListLiveData
 
     fun addEstate(estate: Estate) {
-        val mutableEstateList = estateListLiveData.value?.toMutableList() ?: return
-        mutableEstateList.add(estate)
-        estateListLiveData.value = mutableEstateList.toList()
         CoroutineScope(Dispatchers.IO).launch {
             estateApi.addEstate(estate)
         }
     }
 
     fun updateEstate(estate: Estate) {
-        estateListLiveData.value = estateListLiveData.value?.map {
-            if (it.id == estate.id) estate else it
-        } ?: return
         CoroutineScope(Dispatchers.IO).launch {
             estateApi.updateEstate(estate)
         }
