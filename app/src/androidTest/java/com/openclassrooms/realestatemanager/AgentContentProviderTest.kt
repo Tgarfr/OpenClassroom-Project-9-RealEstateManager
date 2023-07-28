fbd7f64dafd0a065e84b7138e8a7d10e5beb3669
Thomas Garandel <tgarfr@laposte.net>
2023-07-28 17:57:52 +0200

    Add agent content provider

--- app/src/androidTest/java/com/openclassrooms/realestatemanager/AgentContentProviderTest.kt
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/AgentContentProviderTest.kt
@@ -3,34 +3,34 @@ package com.openclassrooms.realestatemanager
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.openclassrooms.realestatemanager.di.Injection
-import com.openclassrooms.realestatemanager.provider.EstateContentProvider
+import com.openclassrooms.realestatemanager.provider.AgentContentProvider
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class ContentProviderTest {
+class AgentContentProviderTest {
 
     private val context = InstrumentationRegistry.getInstrumentation().context
     private val contentResolver = context.contentResolver
-    private val estateRepository = Injection.getInstance(context).estateRepository
+    private val agentRepository = Injection.getInstance(context).agentRepository
 
     @Test
-    fun getEstateListTest() {
+    fun getAgentListTest() {
 
-        val expectedCursor = estateRepository.getEstateListCursor()
+        val expectedCursor = agentRepository.getAgentListCursor()
 
-        val actualCursor = contentResolver.query(EstateContentProvider.URI_ITEM, null, null, null, null)
+        val actualCursor = contentResolver.query(AgentContentProvider.URI_ITEM, null, null, null, null)
 
         Assert.assertTrue(actualCursor != null)
         actualCursor?.let {
 
             Assert.assertTrue(expectedCursor.count == actualCursor.count)
 
-            val columnIndexId = expectedCursor.getColumnIndex("id")
+            val columnIndexId = expectedCursor.getColumnIndex("name")
             while (expectedCursor.moveToNext()) {
                 actualCursor.moveToNext()
-                Assert.assertEquals(expectedCursor.getLong(columnIndexId), actualCursor.getLong(columnIndexId))
+                Assert.assertEquals(expectedCursor.getString(columnIndexId), actualCursor.getString(columnIndexId))
             }
 
         }
